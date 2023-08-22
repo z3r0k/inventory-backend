b2059892ee9bf8660dc14d288e19bdc23fd0569c
Julio Cesar Alvarez Torrecilla <ingjuliocalvarez@gmail.com>
2023-08-21 20:18:01 -0500

    Añadiendo anotacion CrossOrigin

--- src/main/java/com/tecnoservices/inventory/controller/CategoryRestController.java
+++ src/main/java/com/tecnoservices/inventory/controller/CategoryRestController.java
@@ -9,6 +9,7 @@ import com.tecnoservices.inventory.response.CategoryResponseRest;
 import com.tecnoservices.inventory.services.ICategoryServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Janus
  */
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
